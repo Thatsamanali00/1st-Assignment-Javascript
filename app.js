4c9805697a4ffe4beb0df5f1772f82193a3dbc51
Thatsamanali00 <thatsamanali00@gmail.com>
2024-12-17 20:55:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+// Chapter 1
+
+
+// Q1
+//  alert("Hellow,Welcome to Our Web");
+
+//Q2
+ // alert("Error! Please Enter a valid Password");
+
+//Q3
+// alert("Welcome to Js land...\n Happy Coding");
+//Q4
+// alert("Welcome to Js land");
+// alert("Happy Coding");
+// Q5
+// console.log("Hellow I can run Js through my web browser's console");
+
+
+// Chapter 2
+
+// Q1
+// var username= "amaan001";
+
+// Q2
+// var myName= "Amaan Ali";
+
+// Q3
+// var message= "Hellow World";
+// alert(message);
+
+// Q4
+// var myName= "Aman Ali";
+// var age= "16 Years old";
+// var course= "Modern Web and App Development";
+
+// alert(myName);
+// alert(age);
+// alert(course);
+
+// Q5
+// alert("Pizza \n Pizz \n Piz \n Pi \n P");
+
+// Q6
+// var email="thatsamanali00@gmail.com";
+// alert(email);
+
+// Q7
+// var learnJavaScript = "I am trying to learn from the Book A smarter way to learn JavaScript";
+// alert(learnJavaScript);
+
+// Q8
+// document.write("<p> Yah I can write HTML Content through Javascript ");
+
+// Q9
+// var q9= "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(q9);
+
+// Chapter 3
+
+// Q1
+// var age= "I am 16 Years old";
+// alert(age);
+
+// Q2
+// var visitTime= "You have visited this site  14 times ";
+// alert(visitTime);
+
+// Q3
+// document.write("My Birth year is 2006");
+
+
+// Q4
+// var cloth= "Jon Doe ordered 5 T Shirts (s) on XYZ Clothing Store";
+// document.write(cloth)
+
